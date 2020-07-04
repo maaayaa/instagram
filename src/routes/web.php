935590df26189baf60240b66772fb9e66d88c7e3
maaayaa <civichann@gmail.com>
2020-07-04 18:48:01 +0900

    PostsController作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,8 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/post', 'PostsController@index')->name('post');
 
 
 
